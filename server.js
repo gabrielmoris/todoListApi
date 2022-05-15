bd9e91d631c0c4351d156e99737c663915df4bb7
Gabriel C Moris <gabrieltrompeta@gmail.com>
2022-05-15 11:05:40 +0200

    complete controllers, model androutes and put everything together in the server

--- server.js
+++ server.js
@@ -1,7 +1,17 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const Task = require("./api/models/todoListModel");
+const routes = require("./api/routes/todoListRoutes"); //importing route
+const bodyParser = require("body-parser");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+mongoose.Promise = global.Promise;
+mongoose.connect("mongodb://localhost/Tododb");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+routes(app); //register the route
 
 app.listen(PORT, () => {
     console.log(`Server is listenning 👂))) on port: ${PORT}`);
